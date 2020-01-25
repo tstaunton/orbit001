c957593f87bcdad3d6f13aae2a848b7462f42625
Tony Staunton <tony.staunton@gmail.com>
2020-01-25 17:26:49 +0000

    Title Helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+  # Returns a full title on a per page basis
+
+  # Method defintion, optional argument
+  def full_title(page_title = ' ')
+
+    # Creating and assigning a variable
+    base_title = "Orbit Feedback Application"
+
+    # Boolean test
+    if page_title.empty?
+
+      # Implicit return
+      base_title
+    else
+
+      # string concatenation
+      page_title + " | " + base_title
+    end
+  end
 end
